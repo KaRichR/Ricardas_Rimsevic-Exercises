3ae41d147674fa367039ec8133edea27f3073bd6
KaRich <rickarimsevic@gmail.com>
2023-10-02 12:05:35 +0300

    Init commit

---
+++ PD4/JS/main.js
@@ -0,0 +1,15 @@
+let petName;
+let petAge;
+
+petName = "Simba";
+petAge = 3;
+
+console.log("Pet Name: " + petName);
+console.log("Pet Age: " + petAge);
+
+petName = "Rexas";
+petAge = 5;
+
+const buildingAddress = "Gatve 1";
+
+console.log("Building Address: " + buildingAddress);
---
+++ PD5/JS/main.js
@@ -0,0 +1,32 @@
+let number = 12;
+console.log(number);
+console.log(typeof number);
+
+let bigIntNumber = 123456789n;
+console.log(bigIntNumber);
+console.log(typeof bigIntNumber);
+
+let textOne = 'Beautiful JavaScript World';
+console.log(textOne);
+console.log(typeof textOne);
+
+let isTrue = (true == false);
+console.log(isTrue);
+console.log(typeof isTrue);
+
+let petName = undefined;
+console.log(petName);
+console.log(typeof petName);
+
+let petAge = null;
+console.log(petAge);
+console.log(typeof petAge);
+
+let human = {
+  humanName: undefined,
+  humanAge: null
+};
+console.log(humanName);
+console.log(humanAge);
+console.log(typeof human.humanName);
+console.log(typeof human.humanAge);
