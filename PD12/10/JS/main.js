4bb6b1ecf2301030744907f058dd09d322d58983
KaRich <rickarimsevic@gmail.com>
2023-10-08 13:10:24 +0300

    Innit Commit

--- PD12/10/JS/main.js
+++ PD12/10/JS/main.js
@@ -1,6 +1,20 @@
-const p = 5;
-const pz = 50;
-const a = 3;
-const az = 45;
-const k = 3;
-const t = 5;
+function keliasIrLaikas(p, pz, a, az, k, t){
+    const iPrieki = p * pz * k;
+    const atgal = a *az * k;
+    const distancija = iPrieki - atgal;
+
+    const metrais = Math.floor(distancija / 100);
+    const centimetrais = distancija % 100;
+
+    const pilnaDistancija = iPrieki + atgal; 
+
+    const vidGreitis = pilnaDistancija / (t * 100);
+
+    return [metrais, centimetrais, vidGreitis.toFixed(2)];
+}
+
+const input = "5 50 3 45 3 5";
+const inputData = input.split(" ").map(Number);
+
+const [metrais, centimetrais, vidGreitis] = keliasIrLaikas(...inputData);
+console.log(`${metrais} ${centimetrais} ${vidGreitis}`);
