b2ce506881ce15ab642ccd68f97791139bd69067
KaRich <rickarimsevic@gmail.com>
2023-10-10 09:52:58 +0300

    Init commit

--- PD5/JS/main.js
+++ PD5/JS/main.js
@@ -1,14 +1,45 @@
+//1
+let skaicius = 1; 
+console.log(skaicius); 
+//2
+var bigIntSkaicius = 12345678901111111n; 
+console.log(bigIntSkaicius); 
+//3
+var stringas = "Stringas"; 
+console.log(stringas); 
+//4
+var teisingas = true;
+console.log(teisingas); 
+//5
+var kazkas; 
+console.log(kazkas);
+//6
+var nullas = null; 
+console.log(nullas); 
+//7
+var objektas = { vardas: "Ricardas", amzius: 18, miestas: "Vilnius"}; 
+console.log(objektas); 
+//8
+
 let number = 12;
 console.log(number);
 console.log(typeof number);
 
-let bigIntNumber = 123456789n;
-console.log(bigIntNumber);
-console.log(typeof bigIntNumber);
+let otherNumber = 45.89;
+console.log(otherNumber);
+console.log(typeof otherNumber);
 
 let textOne = 'Beautiful JavaScript World';
 console.log(textOne);
 console.log(typeof textOne);
+
+let textTwo = 'Beautiful JavaScript World';
+console.log(textTwo);
+console.log(typeof textTwo);
+
+let textThree = 'Beautiful JavaScript World';
+console.log(textThree);
+console.log(typeof textThree);
 
 let isTrue = (true == false);
 console.log(isTrue);
@@ -26,7 +57,7 @@ let human = {
   humanName: undefined,
   humanAge: null
 };
-console.log(humanName);
-console.log(humanAge);
+console.log(human.humanName);
+console.log(human.humanAge);
 console.log(typeof human.humanName);
 console.log(typeof human.humanAge);
